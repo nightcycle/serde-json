724e5a79c673f1aa9686222c7adcbd0d1866aefd
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2025-04-06 17:18:04 -0600

    npm upgrade

--- README.md
+++
@@ -1,3 +0,0 @@
-# Serde-Json
-
-An attempt to do basic json serialization / deserialization with a single type-build and safe struct definition.
--- scripts/wally-install.sh
+++ scripts/wally-install.sh
@@ -6,10 +6,12 @@ if [ -z "$ROJO_PROJECT" ]; then
   echo "Need to supply project file"
   exit 1
 fi
-
 wally install
 if [ ! -d "Packages" ]; then
   mkdir Packages
+fi
+if [ ! -d "stage-src" ]; then
+  mkdir "stage-src"
 fi
 rojo sourcemap "$ROJO_PROJECT" --output sourcemap.json
 wally-package-types --sourcemap sourcemap.json Packages
